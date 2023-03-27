5f306b39a9204a17b5d914f06e95bd24f16bc6e1
prannav225 <121098142+prannav225@users.noreply.github.com>
2023-03-27 10:29:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TaskTracker from './components/tasktracker';
+
+function App() {
+  return (
+    <div className="App">
+     <TaskTracker/>
+    </div>
+  );
+}
+
+export default App;
